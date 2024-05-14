3e7619384e68adfc66e28357f63d239d7902b145
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-05-14 18:17:09 +0530

    da pre-procurement edit completed

--- router/departmenalAdmin/preProcurement/daPreProcurement.route.ts
+++ router/departmenalAdmin/preProcurement/daPreProcurement.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 const router = express.Router()
-import { backToSr, getPreProcurement, getPreProcurementById, getPreProcurementByOrderNo } from "../../../controller/departmentalAdmin/daPreProcurement.controller";
+import { backToSr, getPreProcurement, getPreProcurementById, getPreProcurementByOrderNo, editPreProcurement } from "../../../controller/departmentalAdmin/daPreProcurement.controller";
 
 
 router.get('/pre-procurement', getPreProcurement)
 router.get('/pre-procurement/:id', getPreProcurementById)
 router.get('/pre-procurement/by-order-no/:order_no', getPreProcurementByOrderNo)
 router.post('/pre-procurement/to-sr', backToSr)
+router.post('/pre-procurement/edit', editPreProcurement)
 
 
 export default router
