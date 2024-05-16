2c01d0def71102383e95464b66f8d12b7537755e
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-05-16 15:36:41 +0530

    export csv

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ const cors = require('cors')
 import masterEntryRoute from './router/masterEntry/masterEntry.route'
 import srPreProcurementRoute from './router/stockReciever/preProcurement/srPreProcurement.route'
 import daPreProcurementRoute from './router/departmenalAdmin/preProcurement/daPreProcurement.route'
+import downloadRoute from './router/download/download.route'
 
 config()
 
@@ -23,6 +24,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/master', masterEntryRoute)
 app.use('/api/sr', srPreProcurementRoute)
 app.use('/api/da', daPreProcurementRoute)
+app.use('/api/download', downloadRoute)
 
 
 app.listen(port, () => {
