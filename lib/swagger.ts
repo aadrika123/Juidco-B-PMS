5b8beb8dfe1d51db4da7db4ba97633685b415fc8
ani_ghost <57623270+anighost1@users.noreply.github.com>
2024-06-08 00:54:10 +0530

    swagger configuration fixed

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -1,11 +1,10 @@
-import { Application } from 'express'
-
-const swaggerJsdoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+import { Application } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const options = {
-    swaggerDefinition: {
-        restapi: '3.0.0',
+    definition: {
+        openapi: '3.0.0',
         info: {
             title: 'Procurement and Inventory Management System',
             version: '1.0.0',
@@ -14,21 +13,23 @@ const options = {
         servers: [
             {
                 url: 'http://localhost:6969',
+                description: "Development server"
             },
             {
-                url: 'https://server.tigga.in/pms/api/pms',
-            }
+                url: 'https://aadrikainfomedia.com/auth',
+                description: "Staging server"
+            },
         ],
     },
-    apis: ['./**/*.ts'],
-}
-
-const specs = swaggerJsdoc(options)
-
-const uiOptions = {
-    customCss: '.swagger-ui .topbar { display: none }'
+    apis: ['**/*.ts'], // Adjust path to match your project structure
 };
 
-export default (app: Application) => {
-    app.use('/api/pms/api-docs', swaggerUi.serve, swaggerUi.setup(specs, uiOptions))
-}
+const specs = swaggerJsdoc(options);
+
+const uiOptions = {
+    customCss: '.swagger-ui .topbar { display: none }',
+};
+
+export default (app: Application): void => {
+    app.use('/api/pms/api-docs', swaggerUi.serve, swaggerUi.setup(specs, uiOptions));
+};
