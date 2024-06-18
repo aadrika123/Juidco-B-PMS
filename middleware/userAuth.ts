64915bb3bc17a118ec0237af9f516dc4f6183b9b
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-06-18 14:53:03 +0530

    user auth and foreign wrapper reconfigured. env changed

--- middleware/userAuth.ts
+++ middleware/userAuth.ts
@@ -1,64 +1,59 @@
-import { PrismaClient } from "@prisma/client";
-import { Request, Response, NextFunction } from "express"
+import { PrismaClient } from '@prisma/client';
+import { Request, Response, NextFunction } from 'express';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const extractRoles = async (userId: number) => {
     const data: any = await prisma.$queryRaw`
     select wf_role_id from wf_roleusermaps where user_id=${userId}
-    `
-    return data.map((item: any) => (item?.wf_role_id))
-}
+    `;
+    return data.map((item: any) => item?.wf_role_id);
+};
 
 export const srAuth = async (req: Request, res: Response, next: NextFunction) => {
-    // const roles = await extractRoles(req?.body?.auth?.id)
-    // if (!roles) {
-    //     res.status(401).send({ error: true, message: "Role ID is required" })
-    // }
-    // if (!roles.includes(59)) {
-    //     res.status(401).send({ error: true, message: "User not authorized to access this api" })
-    // } else {
-    //     next()
-    // }
-    next()
-}
-
+    const roles = await extractRoles(req?.body?.auth?.id);
+    if (!roles) {
+        res.status(401).send({ error: true, message: 'Role ID is required' });
+    }
+    if (!roles.includes(Number(process.env.ROLE_SR))) {
+        res.status(401).send({ error: true, message: 'User not authorized to access this api' });
+    } else {
+        next();
+    }
+};
 
 export const daAuth = async (req: Request, res: Response, next: NextFunction) => {
-    // const roles = await extractRoles(req?.body?.auth?.id)
-    // if (!roles) {
-    //     res.status(401).send({ error: true, message: "Role ID is required" })
-    // }
-    // if (!roles.includes(60)) {
-    //     res.status(401).send({ error: true, message: "User not authorized to access this api" })
-    // } else {
-    //     next()
-    // }
-    next()
-}
-
+    const roles = await extractRoles(req?.body?.auth?.id);
+    if (!roles) {
+        res.status(401).send({ error: true, message: 'Role ID is required' });
+    }
+    if (!roles.includes(Number(process.env.ROLE_DA))) {
+        res.status(401).send({ error: true, message: 'User not authorized to access this api' });
+    } else {
+        next();
+    }
+};
 
 export const accAuth = async (req: Request, res: Response, next: NextFunction) => {
-    const roles = await extractRoles(req?.body?.auth?.id)
+    const roles = await extractRoles(req?.body?.auth?.id);
     if (!roles) {
-        res.status(401).send({ error: true, message: "Role ID is required" })
+        res.status(401).send({ error: true, message: 'Role ID is required' });
     }
-    if (!roles.includes(61)) {
-        res.status(401).send({ error: true, message: "User not authorized to access this api" })
+    if (!roles.includes(Number(process.env.ROLE_ACC))) {
+        res.status(401).send({ error: true, message: 'User not authorized to access this api' });
     } else {
-        next()
+        next();
     }
-}
-
+};
 
 export const distAuth = async (req: Request, res: Response, next: NextFunction) => {
-    const roles = await extractRoles(req?.body?.auth?.id)
+    const roles = await extractRoles(req?.body?.auth?.id);
     if (!roles) {
-        res.status(401).send({ error: true, message: "Role ID is required" })
+        res.status(401).send({ error: true, message: 'Role ID is required' });
     }
-    if (!roles.includes(62)) {
-        res.status(401).send({ error: true, message: "User not authorized to access this api" })
+    if (!roles.includes(Number(process.env.ROLE_DIST))) {
+        res.status(401).send({ error: true, message: 'User not authorized to access this api' });
     } else {
-        next()
+        next();
     }
-}
+};
