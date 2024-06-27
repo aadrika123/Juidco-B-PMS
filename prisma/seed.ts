02d5b33982a6c43f5e9e929a7408290974fc9500
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-06-27 18:36:24 +0530

    seed for product schema added

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,16 +2,18 @@ import { PrismaClient } from '@prisma/client'
 
 import foreign_wrapper from './seeder/foreignWrapper.seed'
 import uuidSetup from './seeder/uuid.seed'
+import productSchema from './seeder/productSchema.seed'
 
 const prisma = new PrismaClient()
 
 async function main() {
 	await foreign_wrapper()
 	await uuidSetup()
+	await productSchema()
 }
 main()
 	.then(async () => {
-    console.log('seed executed')
+		console.log('seed executed')
 		await prisma.$disconnect()
 	})
 	.catch(async e => {
