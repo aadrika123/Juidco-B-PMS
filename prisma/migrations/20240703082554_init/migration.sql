db17f3c4bec1160262f91797fa654ffc8a189f5c
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-07-03 18:14:18 +0530

    Approval and return APIs
    Approval and return APIs for stock return, add to dead stock and warranty claim.
    
    some bug fixes.
    
    remark field in stock_request table

---
+++ prisma/migrations/20240703082554_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "stock_request" ADD COLUMN     "remark" TEXT;
--- router/stockReciever/stockRequest/srStockReq.route.ts
+++ router/stockReciever/stockRequest/srStockReq.route.ts
@@ -2,7 +2,7 @@ import express from 'express'
 // import { upload } from '../../../config/multer.config'
 import { srAuth } from '../../../middleware/userAuth'
 const router = express.Router()
-import { approveStockReq, getStockReqInbox, getStockReqOutbox, rejectStockReq, returnStockReq, stockReturnApproval, deadStockApproval, claimWarranty } from '../../../controller/stockReceiver/srStockRequest.controller'
+import { approveStockReq, getStockReqInbox, getStockReqOutbox, rejectStockReq, returnStockReq, stockReturnApproval, deadStockApproval, claimWarranty, stockReturnReject, stockReturnReqReturn, deadStockReject, deadStockReturn, warrantyClaimReject, warrantyClaimReturn } from '../../../controller/stockReceiver/srStockRequest.controller'
 
 router.use(srAuth)
 
@@ -12,7 +12,13 @@ router.post('/approve', approveStockReq)
 router.post('/return', returnStockReq)
 router.post('/reject', rejectStockReq)
 router.post('/approve-return-inv', stockReturnApproval)
+router.post('/reject-return-inv', stockReturnReject)
+router.post('/return-return-inv', stockReturnReqReturn)
 router.post('/approve-dead-stock', deadStockApproval)
+router.post('/reject-dead-stock', deadStockReject)
+router.post('/return-dead-stock', deadStockReturn)
 router.post('/approve-warranty', claimWarranty)
+router.post('/reject-warranty', warrantyClaimReject)
+router.post('/return-warranty', warrantyClaimReturn)
 
 export default router
