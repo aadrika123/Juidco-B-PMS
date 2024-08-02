73887f075c039a9771ee492268bf6d3d7c1cc54e
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-08-02 13:23:37 +0530

    pre tender details APIs (create and get)

--- controller/preTender/preTender.controller.ts
+++ controller/preTender/preTender.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import {
     getPreTenderDal,
+    createPreTenderDetailsDal,
+    getPreTenderDetailsDal
 } from "../../dal/preTender/preTender.dal";
 
 
@@ -16,6 +18,40 @@ export const getPreTender = async (req: Request, res: Response) => {
         res.status(404).json({
             status: false,
             message: `Error while fetching pre tender`,
+            error: result?.message
+        })
+    }
+}
+
+export const createPreTenderDetails = async (req: Request, res: Response) => {
+    const result: any = await createPreTenderDetailsDal(req)
+    if (!result?.error) {
+        res.status(201).json({
+            status: true,
+            message: `Pre tender details created successfully`,
+            data: result
+        })
+    } else {
+        res.status(400).json({
+            status: false,
+            message: `Error while Pre tender details creation`,
+            error: result?.message
+        })
+    }
+}
+
+export const getPreTenderDetails = async (req: Request, res: Response) => {
+    const result: any = await getPreTenderDetailsDal(req)
+    if (!result?.error) {
+        res.status(201).json({
+            status: true,
+            message: `Pre tender details fetched successfully`,
+            data: result
+        })
+    } else {
+        res.status(400).json({
+            status: false,
+            message: `Error while fetching Pre tender details`,
             error: result?.message
         })
     }
--- router/preTender/preTender.route.ts
+++ router/preTender/preTender.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 const router = express.Router()
 import {
-    getPreTender
+    getPreTender,
+    createPreTenderDetails,
+    getPreTenderDetails
 } from "../../controller/preTender/preTender.controller";
 
 
+router.post('/details', createPreTenderDetails)
+router.get('/details/:reference_no', getPreTenderDetails)
 router.get('/:reference_no', getPreTender)
 
 
