c8d1802ed415afbe81f27aa49f7a7224e35be054
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-08-07 13:46:10 +0530

    addition of multiple criteria types

--- router/tenderingAdmin/ta.route.ts
+++ router/tenderingAdmin/ta.route.ts
@@ -5,6 +5,7 @@ import {
     getTaOutbox,
     selectBidType,
     addCriteria,
+    submitCriteria,
     addBidderDetails,
     submitBidderDetails
 } from "../../controller/tenderingAdmin/ta.controller";
@@ -14,7 +15,8 @@ import { upload } from "../../config/multer.config";
 router.get('/', getTaInbox)
 router.get('/outbox', getTaOutbox)
 router.post('/bid-type', selectBidType)
-router.post('/criteria', addCriteria)
+router.post('/add-criteria', addCriteria)
+router.post('/submit-criteria', submitCriteria)
 router.post('/add-bidder', upload.fields([{ name: 'emd_doc' }, { name: 'bidder_doc' }]), addBidderDetails)
 router.post('/submit-bidder', submitBidderDetails)
 
