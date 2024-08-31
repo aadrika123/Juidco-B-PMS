1b70e6fcb3faacd4d825bd1092a44d1362576d3a
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-08-31 17:36:06 +0530

    DD side emp service request (except approval)

--- router/distributor/distServiceRequest.route.ts
+++ router/distributor/distServiceRequest.route.ts
@@ -2,11 +2,18 @@ import express from 'express'
 import { distAuth } from '../../middleware/userAuth'
 const router = express.Router()
 import { createServiceRequest, getServiceReqInbox, getServiceReqOutbox } from '../../controller/distributor/distServiceRequest.controller'
+import { getEmpServiceReqInbox, getEmpServiceReqOutbox, approveEmpServiceRequest, rejectServiceRequest } from '../../controller/distributor/distEmpServiceRequest.controller'
 
 router.use(distAuth)
 
 router.post('/', createServiceRequest)
 router.get('/', getServiceReqInbox)
 router.get('/outbox', getServiceReqOutbox)
+
+//Employee service request
+router.post('/emp/approve', approveEmpServiceRequest)
+router.post('/emp/reject', rejectServiceRequest)
+router.get('/emp', getEmpServiceReqInbox)
+router.get('/emp/outbox', getEmpServiceReqOutbox)
 
 export default router
