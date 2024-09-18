f09e6cc90fda3527bf96c05aaf2d437230b818ee
Anil Tigga <anil.anil300@gmail.com>
2024-09-18 23:55:43 +0530

    dashboard data updated

--- controller/dashboard/distDashboard.controller.ts
+++ controller/dashboard/distDashboard.controller.ts
@@ -1,16 +1,24 @@
 import { Request, Response } from "express";
 import {
-    getInventoryDashboardDal
+    getInventoryDashboardDal,
+    getAssignedStocksDal,
+    getCountsDal
 } from "../../dal/dashboard/inventoryDashboard.dal";
 
 
 export const getDistDashboard = async (req: Request, res: Response) => {
     try {
-        const result: any = await getInventoryDashboardDal()
+        const returnData = await getInventoryDashboardDal('return')
+        const deadData = await getInventoryDashboardDal('dead')
+        const assignedData = await getAssignedStocksDal()
+        const counts = await getCountsDal()
         res.status(200).json({
             status: true,
             message: `Distributor dashboard data fetched successfully`,
-            data: result
+            data: {
+                graph: { returnData, deadData, assignedData },
+                counts
+            }
         })
     } catch (err: any) {
         res.status(404).json({
--- dal/dashboard/inventoryDashboard.dal.ts
+++ dal/dashboard/inventoryDashboard.dal.ts
@@ -1,14 +1,13 @@
-import { Request } from 'express'
 import getErrorMessage from '../../lib/getErrorMessage'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, service_enum } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-function getWeekNumber(date: Date): number {
-	const firstDayOfYear = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
-	const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
-	return Math.ceil((pastDaysOfYear + firstDayOfYear.getUTCDay() + 1) / 7);
-}
+// function getWeekNumber(date: Date): number {
+// 	const firstDayOfYear = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+// 	const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+// 	return Math.ceil((pastDaysOfYear + firstDayOfYear.getUTCDay() + 1) / 7);
+// }
 
 function getFormattedDateMMDD(date: Date): string {
 	const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // months are zero-indexed
@@ -16,47 +15,8 @@ function getFormattedDateMMDD(date: Date): string {
 	return `${month}-${day}`;
 }
 
-export const getInventoryDashboardDal = async () => {
+export const getInventoryDashboardDal = async (service: service_enum) => {
 	try {
-
-		// const today = new Date();
-
-		// const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 1));
-		// startOfWeek.setHours(0, 0, 0, 0);
-
-		// const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7));
-		// endOfWeek.setHours(23, 59, 59, 999);
-
-		// const dailyCountForWeek = await prisma.service_request.groupBy({
-		// 	by: ['createdAt'],
-		// 	_count: {
-		// 		_all: true,
-		// 	},
-		// 	where: {
-		// 		createdAt: {
-		// 			gte: new Date('2024-09-01'),
-		// 			lte: new Date('2024-09-10'),
-		// 		},
-		// 	},
-		// 	orderBy: {
-		// 		createdAt: 'asc',
-		// 	},
-		// });
-
-		// const groupedData = dailyCountForWeek.reduce((acc: any, entry) => {
-		// 	const date = new Date(entry.createdAt).toISOString().split('T')[0]; // Extract only the date (YYYY-MM-DD)
-
-		// 	if (!acc[date]) {
-		// 		acc[date] = { date: date, count: 0 }; // Initialize if date not in accumulator
-		// 	}
-
-		// 	acc[date].count += entry._count._all; // Sum the count for the same date
-
-		// 	return acc;
-		// }, {});
-		// const weekly = Object.values(groupedData);
-
-
 		interface GroupedData {
 			week?: string;
 			month?: string;
@@ -75,7 +35,7 @@ export const getInventoryDashboardDal = async () => {
 		const getData = async () => {
 			return await prisma.service_request.groupBy({
 				where: {
-					service: 'return'
+					service: service
 				},
 				by: ['createdAt'],
 				_count: {
@@ -129,6 +89,140 @@ export const getInventoryDashboardDal = async () => {
 		const data: any = await getData()
 
 		return groupByPeriod(data)
+	} catch (err: any) {
+		console.log(err)
+		return { error: true, message: getErrorMessage(err) }
+	}
+}
+
+export const getAssignedStocksDal = async () => {
+	try {
+		interface GroupedData {
+			week?: string;
+			month?: string;
+			year?: number;
+			count: number;
+		}
+
+		interface ServiceRequestData {
+			createdAt: string; // Prisma returns dates as strings in ISO format
+			_count: {
+				_all: number;
+			};
+		}
+
+		// Run the query to get the createdAt field and the counts
+		const getData = async () => {
+			return await prisma.stock_handover.groupBy({
+				by: ['createdAt'],
+				_count: {
+					_all: true,
+				},
+			});
+		};
+
+		// Function to group by weekly, monthly, and yearly
+		function groupByPeriod(data: ServiceRequestData[]): {
+			weekly: GroupedData[];
+			monthly: GroupedData[];
+			yearly: GroupedData[];
+		} {
+			const weekly: Record<string, GroupedData> = {};
+			const monthly: Record<string, GroupedData> = {};
+			const yearly: Record<number, GroupedData> = {};
+
+			data.forEach((entry) => {
+				const date = new Date(entry.createdAt);
+
+				// Group by week (ISO week number)
+				const week = getFormattedDateMMDD(date);
+				if (!weekly[week]) {
+					weekly[week] = { week: week, count: 0 };
+				}
+				weekly[week].count += entry._count._all;
+
+				// Group by month (YYYY-MM)
+				const month = date.toISOString().split('T')[0].slice(0, 7);
+				if (!monthly[month]) {
+					monthly[month] = { month: month, count: 0 };
+				}
+				monthly[month].count += entry._count._all;
+
+				// Group by year (YYYY)
+				const year = date.getUTCFullYear();
+				if (!yearly[year]) {
+					yearly[year] = { year: year, count: 0 };
+				}
+				yearly[year].count += entry._count._all;
+			});
+
+			return {
+				weekly: Object.values(weekly),
+				monthly: Object.values(monthly),
+				yearly: Object.values(yearly),
+			};
+		}
+
+		const data: any = await getData()
+
+		return groupByPeriod(data)
+	} catch (err: any) {
+		console.log(err)
+		return { error: true, message: getErrorMessage(err) }
+	}
+}
+
+export const getCountsDal = async () => {
+	try {
+
+		const schema: any = await prisma.$queryRawUnsafe(`
+			SELECT table_name
+			FROM information_schema.tables
+			WHERE table_schema = 'product'
+		  `);
+
+		if (schema?.length === 0) {
+			throw new Error('No product')
+		}
+
+		const total: any[] = await prisma.$queryRawUnsafe(
+			`
+			SELECT SUM(opening_quantity) AS total_sum
+			FROM ${schema.map((table: any) => table.table_name).join(' UNION ALL SELECT SUM(opening_quantity) AS total_sum FROM ')}
+		`);
+
+		const remaining: any[] = await prisma.$queryRawUnsafe(
+			`
+			SELECT SUM(quantity) AS total_sum
+			FROM ${schema.map((table: any) => table.table_name).join(' UNION ALL SELECT SUM(quantity) AS total_sum FROM ')}
+		`);
+
+		const dead = await prisma.inventory_dead_stock.aggregate({
+			_sum: {
+				quantity: true
+			}
+		})
+
+		const movement = await prisma.stock_req_product.aggregate({
+			where: {
+				stock_request: {
+					status: {
+						notIn: [-2, -1, 0, 1, 2, 80, 81, 82]
+					}
+				}
+			},
+			_sum: {
+				quantity: true
+			}
+		})
+
+		return {
+			totalStocks: total[0].total_sum,
+			remainingStocks: remaining[0].total_sum,
+			deadStocks: dead?._sum?.quantity,
+			stocksInMovement: movement?._sum?.quantity
+		}
+
 	} catch (err: any) {
 		console.log(err)
 		return { error: true, message: getErrorMessage(err) }
