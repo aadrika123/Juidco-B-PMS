8f63407a1790b16d499825cb0654383802b5e98b
Anil Tigga <57623270+anighost1@users.noreply.github.com>
2024-11-14 17:47:08 +0530

    delete migration

--- prisma/migrations/20241114120310_rate_contract_supplier_relation/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `rate_contract_supplierId` on the `procurement` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "procurement" DROP COLUMN "rate_contract_supplierId";
