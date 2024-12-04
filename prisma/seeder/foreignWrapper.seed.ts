dc5d75be0ba7d1f380665a4e8922b9f3647ba201
root <it.info@aadrikaenterprises.com>
2024-12-04 12:55:22 +0530

    changes made

--- prisma/seeder/foreignWrapper.seed.ts
+++ prisma/seeder/foreignWrapper.seed.ts
@@ -4,13 +4,10 @@ const prisma = new PrismaClient();
 
 const foreign_wrapper = async () => {
     await prisma.$queryRaw`CREATE EXTENSION IF NOT EXISTS postgres_fdw`;
-<<<<<<< HEAD
+
     await prisma.$queryRaw`CREATE SERVER master_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '172.18.1.50', port '5432', dbname 'juidco_masters')`;
     await prisma.$queryRaw`CREATE USER MAPPING FOR postgres SERVER master_fdw OPTIONS (user 'postgres', password 'Perfect@#@##)@?')`;
-=======
-    await prisma.$queryRaw`CREATE SERVER master_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'localhost', port '5432', dbname 'juidco_masters')`;
-    await prisma.$queryRaw`CREATE USER MAPPING FOR postgres SERVER master_fdw OPTIONS (user 'postgres', password '0000')`;
->>>>>>> refs/remotes/origin/main
+
     await prisma.$queryRaw`GRANT USAGE ON FOREIGN SERVER master_fdw TO postgres`;
     await prisma.$queryRaw`IMPORT FOREIGN SCHEMA public LIMIT TO (users, wf_roles, wf_roleusermaps, ulb_masters) FROM SERVER master_fdw INTO public`;
 };
